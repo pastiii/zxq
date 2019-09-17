54e4ed39b57aa91ae2bac601b29d86ff8c9f2f81
zxq <zhuxiaoqiang@zhehekeji.com>
2019-09-17 11:37:08 +0800

    'addapm'

---
+++ apm.php
@@ -0,0 +1,16 @@
+<?
+#修改配置
+vim /etc/docker/daemon.json
+
+#json内容
+{
+    "insecure-registries":["121.199.53.9:5000"]  #配置多个仓库地址一逗号连接 例如:"harbor.test.com","registry.cn-shenzhen.aliyuncs.com"
+}
+
+#启用配置
+systemctl daemon-reload
+systemctl restart docker
+
+#拉取镜像
+docker pull 121.199.53.9:5000/apm-ubuntu:latest
+
